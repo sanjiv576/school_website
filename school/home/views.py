179526d6134d2c7b86c516e37cfaf655f151bfa1
sanjiv576 <sanjiv576>
2022-07-23 00:42:04 +0545

    retrieved data from database

--- school/home/views.py
+++ school/home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from user.models import Notice_Vacancy
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/homepage.html')
+
+    notice_details = Notice_Vacancy.objects.all()
+    return render(request, 'home/homepage.html', {'notice_details': notice_details})
 
 
 # def register(request):
@@ -10,4 +14,6 @@ def index(request):
 
 
 def home(request):
-    return render(request, "home/homepage.html")
+    
+    notice_details = Notice_Vacancy.objects.all()
+    return render(request, 'home/homepage.html', {'notice_details': notice_details})
