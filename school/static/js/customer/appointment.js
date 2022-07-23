8f597fa3d039cfe005ba01f94a3aef5cbc944461
sanjiv576 <sanjiv576>
2022-07-23 10:11:20 +0545

    added appointment section

---
+++ school/static/js/customer/appointment.js
@@ -0,0 +1,8 @@
+function sendAppointment(){
+    Swal.fire({
+        icon: 'success',
+        title: 'Congratulations !',
+        text: 'Your appointment has been scheduled. Please visit office.',
+        
+      })
+}
